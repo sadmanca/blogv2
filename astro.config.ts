15b6ecc63a34b7925ef1221d36fb36c16df7f91e
Sadman Hossain <m.sadman.h@gmail.com>
2024-11-15 03:40:11 +0000

    add: unpic image service for blurred image previews

--- astro.config.ts
+++ astro.config.ts
@@ -20,6 +20,7 @@ import sectionize from '@hbsnow/rehype-sectionize'
 import icon from 'astro-icon'
 
 import partytown from '@astrojs/partytown'
+import { imageService } from "@unpic/astro/service";
 
 // https://astro.build/config
 export default defineConfig({
@@ -38,6 +39,11 @@ export default defineConfig({
       },
     }),
   ],
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+    }),
+  },
   redirects: {
     '/uoft-pey-coop-jobs-2023': '/uoft-pey-coop-jobs-2023.html',
     '/uoft-work-study-2024': '/uoft-work-study-2024.html',
