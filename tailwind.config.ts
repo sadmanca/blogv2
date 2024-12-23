7f06c72100f25007b0148572bec8c028638e9a31
Sadman Hossain <m.sadman.h@gmail.com>
2024-12-23 00:56:18 +0000

    style: update body font to Inter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Geist', ...defaultTheme.fontFamily.sans],
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
         mono: ['Geist Mono', ...defaultTheme.fontFamily.mono],
       },
       colors: {
