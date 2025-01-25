a17a241ff4d51df4ed1e3ebc4b1065f357571ba5
Sadman Hossain <m.sadman.h@gmail.com>
2025-01-25 13:38:22 -0500

    feat: update TraktGrid to use astro content loader

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,37 @@
 import { defineCollection, z } from 'astro:content'
+import { RateLimiter } from 'limiter'
 import xml2js from 'xml2js'
+
+const TRAKT_WATCHED_URL = `https://api.trakt.tv/users/sadmanca/watched`
+const TRAKT_RATINGS_URL = `https://api.trakt.tv/users/sadmanca/ratings`
+const TRAKT_CLIENT_ID = import.meta.env.TRAKT_CLIENT_ID
+const TMDB_API_KEY = import.meta.env.TMDB_API_KEY
+
+const limiter_trakt = new RateLimiter({ tokensPerInterval: 1, interval: 'second' })
+const limiter_tmdb = new RateLimiter({ tokensPerInterval: 50, interval: 'second' })
+
+async function fetchWithRetry(url: string, type: string, options = {}) {
+  let response;
+  let retries = 5;
+
+  while (retries > 0) {
+    if (type == 'trakt') {
+      await limiter_trakt.removeTokens(1);
+    } else {
+      await limiter_tmdb.removeTokens(1);
+    }
+    response = await fetch(url, options);
+
+    if (response.status !== 429) {
+      return response;
+    }
+
+    retries -= 1;
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait 1 second before retrying
+  }
+
+  throw new Error('Too many requests, please try again later.');
+}
 
 const goodreads_read_books = defineCollection({
   schema: z.object({
@@ -34,6 +66,138 @@ const goodreads_read_books = defineCollection({
     });
 
     return goodreads_read_books;
+  }
+});
+
+const trakt_watched_movies = defineCollection({
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    year: z.number(),
+    rating: z.number(),
+    last_watched_at: z.string(),
+    poster: z.string(),
+    imdb: z.string(),
+  }),
+  loader: async () => {
+    const type = 'movie'
+    const alt_type = 'movies'
+    const alt_type2 = 'movie'
+    
+    const watchedResponse = await fetchWithRetry(`${TRAKT_WATCHED_URL}/${alt_type}`, "trakt", {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': TRAKT_CLIENT_ID,
+      },
+    });
+    const watchedData = await watchedResponse.json();
+
+    const ratingsResponse = await fetchWithRetry(`${TRAKT_RATINGS_URL}/${alt_type}`, "trakt", {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': TRAKT_CLIENT_ID,
+      },
+    });
+    const ratingsData = await ratingsResponse.json();
+
+    const ratings = ratingsData.reduce(
+      (
+        acc: { [x: string]: any },
+        item: { [x: string]: { ids: { tmdb: string | number } }; rating: any },
+      ) => {
+        acc[item[type].ids.tmdb] = item.rating
+        return acc
+      },
+      {},
+    );
+
+    const movies = await Promise.all(watchedData.map(async (item: any) => {
+      const tmdbId = item[type].ids.tmdb;
+      const image_api_request = `https://api.themoviedb.org/3/${alt_type2}/${tmdbId}?api_key=${TMDB_API_KEY}&language=en-US`;
+
+      const tmdbResponse = await fetchWithRetry(image_api_request, "tmdb");
+      const tmdbData = await tmdbResponse.json();
+
+      return {
+        id: item[type].ids.imdb,
+        title: item[type].title,
+        year: item[type].year,
+        rating: ratings[tmdbId] || 0,
+        last_watched_at: item.last_watched_at,
+        poster: `https://image.tmdb.org/t/p/w200${tmdbData.poster_path}`,
+        imdb: item[type].ids.imdb
+      };
+    }));
+
+    return movies;
+  }
+});
+
+const trakt_watched_shows = defineCollection({
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    year: z.number(),
+    rating: z.number(),
+    last_watched_at: z.string(),
+    poster: z.string(),
+    imdb: z.string(),
+  }),
+  loader: async () => {
+    const type = 'show'
+    const alt_type = 'shows'
+    const alt_type2 = 'tv'
+    
+    const watchedResponse = await fetchWithRetry(`${TRAKT_WATCHED_URL}/${alt_type}`, "trakt", {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': TRAKT_CLIENT_ID,
+      },
+    });
+    const watchedData = await watchedResponse.json();
+
+    const ratingsResponse = await fetchWithRetry(`${TRAKT_RATINGS_URL}/${alt_type}`, "trakt", {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': TRAKT_CLIENT_ID,
+      },
+    });
+    const ratingsData = await ratingsResponse.json();
+
+    const ratings = ratingsData.reduce(
+      (
+        acc: { [x: string]: any },
+        item: { [x: string]: { ids: { tmdb: string | number } }; rating: any },
+      ) => {
+        acc[item[type].ids.tmdb] = item.rating
+        return acc
+      },
+      {},
+    );
+
+    const shows = await Promise.all(watchedData.map(async (item: any) => {
+      const tmdbId = item[type].ids.tmdb;
+      const image_api_request = `https://api.themoviedb.org/3/${alt_type2}/${tmdbId}?api_key=${TMDB_API_KEY}&language=en-US`;
+
+      const tmdbResponse = await fetchWithRetry(image_api_request, "tmdb");
+      const tmdbData = await tmdbResponse.json();
+
+      return {
+        id: item[type].ids.imdb,
+        title: item[type].title,
+        year: item[type].year,
+        rating: ratings[tmdbId] || 0,
+        last_watched_at: item.last_watched_at,
+        poster: `https://image.tmdb.org/t/p/w200${tmdbData.poster_path}`,
+        imdb: item[type].ids.imdb
+      };
+    }));
+
+    return shows;
   }
 });
 
@@ -105,4 +269,11 @@ const projects = defineCollection({
     }),
 })
 
-export const collections = { posts, authors, projects, goodreads_read_books }
+export const collections = { 
+  posts,
+  authors, 
+  projects, 
+  goodreads_read_books, 
+  trakt_watched_movies,
+  trakt_watched_shows
+}
