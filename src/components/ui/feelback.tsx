06e59f9cc34d710dd95f0e659600f04c7d82e0b7
Sadman Hossain <m.sadman.h@gmail.com>
2025-02-07 19:44:36 -0500

    style(post): feelback only show hearts

--- src/components/ui/feelback.tsx
+++ src/components/ui/feelback.tsx
@@ -2,17 +2,10 @@ import { FeelbackPulse, PRESET_PULSE_HEART, FeelbackYesNo, PRESET_UP_DOWN_VOTE }
 
 export default function Feelback() {
   return (
-    <div className="container flex flex-wrap justify-center ml-5 gap-10">
-      <FeelbackPulse
-        contentSetId="3574bcc4-bcec-4cca-8849-f94e570edf04"
-        preset={PRESET_PULSE_HEART}
-        showCount
-      />
-    <FeelbackYesNo
-        contentSetId="c2a26718-b0da-4fd7-a4a6-b773fe00ba9d"
-        preset={PRESET_UP_DOWN_VOTE}
-        showCount
+    <FeelbackPulse
+      contentSetId="3574bcc4-bcec-4cca-8849-f94e570edf04"
+      preset={PRESET_PULSE_HEART}
+      showCount
     />
-    </div>
   );
 }
